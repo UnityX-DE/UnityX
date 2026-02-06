7fe21fa4c8e90604008e22a08c2b183fd8967580
MONCHOBRONCHO <MONCHOBRONCHO@proton.me>
2026-02-06 17:31:42 -0500

    Add src/main.py.

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import sys
+from compositor.compositor import UnityXCompositor
+
+def main():
+    try:
+        unityx = UnityXCompositor()
+        unityx.run()
+    except KeyboardInterrupt:
+        print("\nUnityX shut down by user.")
+        sys.exit(0)
+    except Exception as e:
+        print(f"UnityX failed to start: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
