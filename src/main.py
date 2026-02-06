24a5b15d65c476c9c3a39e617d1bf8f4ab1d2f59
MONCHOBRONCHO <MONCHOBRONCHO@proton.me>
2026-02-06 17:37:15 -0500

    Fix broad exception handling in main.py.

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ def main():
     except KeyboardInterrupt:
         print("\nUnityX shut down by user.")
         sys.exit(0)
-    except Exception as e:
+    except Exception as e: # pylint: disable=broad-exception-caught
         print(f"UnityX failed to start: {e}")
         sys.exit(1)
 
